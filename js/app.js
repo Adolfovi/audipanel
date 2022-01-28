1360f9c07ca0fb412840cb523b80095c9505e86d
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2022-01-28 20:02:02 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var sribai = new Audio('audio/gilipollas.mp3');
+var srpig = new Audio('audio/gorrino.mp3');
+var srmiquel = new Audio('audio/ostiapilotes.mp3');
+var srfart = new Audio('audio/pedo.mp3');
+
+
+function isibai(){
+    sribai.play();
+    sribai.loop() = false;
+}
+
+function ispig(){
+    srpig.play();
+    srpig.loop() = false;
+}
+
+function ismiquel(){
+    srmiquel.play();
+    srmiquel.loop() = false;
+}
+
+function isfart(){
+    srfart.play();
+    srfart.loop() = false;
+}
